6b97905d75a253b06d35cb5fededb80363f82777
Malek-Jabareen <malekj94@gmail.com>
2020-06-12 16:29:32 +0300

    Spring Application Initialization

---
+++ src/main/java/com/malek/exam/mabaya/online_sponsored_ads/OnlineSponsoredAdsApplication.java
@@ -0,0 +1,13 @@
+package com.malek.exam.mabaya.online_sponsored_ads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSponsoredAdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineSponsoredAdsApplication.class, args);
+    }
+
+}
