851e7e1038ed1a76a6ae4d70c8bf6602e2d2ffe8
Malek-Jabareen <malekj94@gmail.com>
2020-06-14 23:19:56 +0300

    CORS: Option => Options

--- src/main/java/com/malek/exam/mabaya/online_sponsored_ads/configs/CorsConfig.java
+++ src/main/java/com/malek/exam/mabaya/online_sponsored_ads/configs/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowedOrigins("*");
             }
